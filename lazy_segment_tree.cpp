2bf29a4243ce3efdfa966b41f0a13ed5dca900b7
AbdulRahmanLuai <129676787+AbdulRahmanLuai@users.noreply.github.com>
2024-09-01 01:12:57 +0300

    Update lazy_segment_tree.cpp
    updated

--- lazy_segment_tree.cpp
+++ lazy_segment_tree.cpp
@@ -1,106 +1,115 @@
 #include <bits/stdc++.h>
-#include <iostream>  
-using namespace std;  
+using namespace std;
+
 #define ll long long
 #define endl "\n"
 #define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
- 
- 
- 
-const ll MOD = 998244353;
-const ll INF = 1e9;
+
+const ll INF = 1e18;
 const ll N = 2e5 + 100;
-int a[N];
-ll seg[4*N];
-ll lazy[4*N];
+
+ll n, q;
+ll a[N];
+ll seg[4 * N];
+ll lazy[4 * N];
 ll defaultValue = 0;
 
-ll combine(ll a, ll b){
+ll combine(ll a, ll b) {
     return a + b;
 }
 
-void build(int id, int leftNode, int rightNode){
-    if (leftNode == rightNode){
+void build(ll id, ll leftNode, ll rightNode) {
+    if (leftNode == rightNode) {
         seg[id] = a[leftNode];
         return;
     }
-    int mid = (leftNode + rightNode) >> 1;
-    build(id*2, leftNode, mid);
-    build(id*2 + 1, mid + 1, rightNode);
+    ll mid = (leftNode + rightNode) >> 1;
+    build(id * 2, leftNode, mid);
+    build(id * 2 + 1, mid + 1, rightNode);
 
-    seg[id] = combine(seg[id*2], seg[id*2 + 1]);
+    seg[id] = combine(seg[id * 2], seg[id * 2 + 1]);
 }
 
-void updateRange(int id, int leftNode, int rightNode, int updateLeft, int updateRight, int value){
-    if (updateRight < leftNode || updateLeft > rightNode){
-        return;
+void push(ll id, ll leftNode, ll rightNode) {
+    if (lazy[id] != 0) {
+        seg[id] += lazy[id] * (rightNode - leftNode + 1); // make sure this is correct for different problems. 
+
+        if (leftNode != rightNode) {
+            lazy[id * 2] += lazy[id];
+            lazy[id * 2 + 1] += lazy[id];
+        }
+        lazy[id] = 0;
     }
-    if (leftNode >= updateLeft && rightNode <= updateRight){
-        lazy[id] += value;
-        return;
-    }
-    int mid = (leftNode + rightNode) >> 1;
-    updateRange(2*id, leftNode, mid, updateLeft, updateRight, value);
-    updateRange(2*id + 1, mid + 1, rightNode, updateLeft, updateRight, value);
 }
 
-ll query(int id, int leftNode, int rightNode, int queryLeft, int queryRight){
+void updateRange(ll id, ll leftNode, ll rightNode, ll updateLeft, ll updateRight, ll value) {
+    push(id, leftNode, rightNode);
 
-    if (leftNode > queryRight || rightNode < queryLeft){
+    if (updateRight < leftNode || updateLeft > rightNode) {
+        return;
+    }
+    if (leftNode >= updateLeft && rightNode <= updateRight) {
+        
+        lazy[id] += value; 
+        push(id, leftNode, rightNode);
+        return;
+    }
+
+    ll mid = (leftNode + rightNode) >> 1;
+    updateRange(id * 2, leftNode, mid, updateLeft, updateRight, value);
+    updateRange(id * 2 + 1, mid + 1, rightNode, updateLeft, updateRight, value);
+
+    seg[id] = combine(seg[id * 2], seg[id * 2 + 1]);
+}
+
+ll query(ll id, ll leftNode, ll rightNode, ll queryLeft, ll queryRight) {
+    push(id, leftNode, rightNode);
+
+    if (queryRight < leftNode || queryLeft > rightNode) {
         return defaultValue;
     }
-    if (leftNode >= queryLeft && rightNode <= queryRight){
-        return seg[id] + lazy[id] * (rightNode - leftNode + 1);
+    if (leftNode >= queryLeft && rightNode <= queryRight) {
+        return seg[id];
     }
-    
-    int mid = (leftNode + rightNode) >> 1;
-    lazy[id*2] += lazy[id];
-    lazy[id*2 + 1] += lazy[id];
-    
-    ll leftAnswer = query(id*2, leftNode, mid, queryLeft, queryRight);
+
+    ll mid = (leftNode + rightNode) >> 1;
+    ll leftAnswer = query(id * 2, leftNode, mid, queryLeft, queryRight);
     ll rightAnswer = query(id * 2 + 1, mid + 1, rightNode, queryLeft, queryRight);
-    seg[id] = combine(leftAnswer, rightAnswer);
-    lazy[id] = 0;
-    return seg[id];
+
+    return combine(leftAnswer, rightAnswer);
 }
 
-
-
-
-void solve(){
-    int n, q; cin >> n >> q;
+void solve() {
+    cin >> n >> q;
     for (int i=1; i<=n; i++) cin >> a[i];
     build(1, 1, n);
 
     while(q--){
-        int type; cin >> type;
-
-        if(type == 1){
-            int l, r, u; cin >> l >> r >> u;
-            updateRange(1, 1, n, l, r, u);
+        int t; cin >> t;
+        if (t == 1){
+        int l, r, u; cin >> l >> r >> u;
+        updateRange(1, 1, n, l, r, u);
         }
         else{
             int k; cin >> k;
             cout << query(1, 1, n, k, k) << endl;
+        
         }
     }
+    
+
 }
-
-
 
 int main() {
     fastIO
- 
- 
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "rt", stdin);
     freopen("outputs.txt", "wt", stdout);
     #endif
- 
+
     ll T = 1;
     // cin >> T;
     while (T--) solve();
-    
-    
+
     return 0;
-} 
+}
