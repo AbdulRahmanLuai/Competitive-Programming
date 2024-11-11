d0e0cb21b03df81ad5e9b8a733abde6d0b5fd4e3
AbdulRahmanLuai <129676787+AbdulRahmanLuai@users.noreply.github.com>
2024-11-12 00:53:13 +0300

    Update ordered_set.cpp

--- ordered_set.cpp
+++ ordered_set.cpp
@@ -5,3 +5,7 @@ typedef tree<int,null_type,less<int>,rb_tree_tag,
                 tree_order_statistics_node_update> ordered_set;
 
 ordered_set st;
+
+
+auto x = st.find_by_order(2);
+st.order_of_key(6);
